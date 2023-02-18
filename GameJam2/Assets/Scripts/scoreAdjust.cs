7a3c302c7b6c31a3f6cff0cb91f1198e8e5d3129
mmayans <82738387+mmayans@users.noreply.github.com>
2023-02-18 13:13:57 -0700

    separated assets, reorganized room

--- GameJam2/Assets/Scripts/scoreAdjust.cs
+++ GameJam2/Assets/Scripts/scoreAdjust.cs
@@ -6,9 +6,11 @@ using TMPro;
 public class scoreAdjust : MonoBehaviour
 {
     [SerializeField] int score;
-    
+    bool fall = false;
     public TMP_Text txt;
-   
+    public AudioSource splash;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,17 +19,27 @@ public class scoreAdjust : MonoBehaviour
 
     void OnTriggerEnter(Collider coll)
     {
+        
        
         if (coll.gameObject.tag == "ingredient")
         {
+            splash.Play();
             score = score + 5;
             Destroy(coll.gameObject);
         }
         if (coll.gameObject.tag == "zombie")
         {
+            splash.Play();
             score = score - 1;
             Destroy(coll.gameObject);
             
+        }
+       if (coll.gameObject.tag == "player")
+        {
+            splash.Play();
+            score = score - 1;
+            fall = true;
+
         }
 
 
@@ -38,6 +50,10 @@ public class scoreAdjust : MonoBehaviour
     void Update()
     {
         txt.text = "score: " + score.ToString();
+        if(fall == true)
+        {
+            Debug.Log("You can't go in the soup, silly!");
+        }
         
         
 
